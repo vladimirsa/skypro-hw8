1bd9aa4f5f90dc1f061c13e02908e679eaca1b47
Vladimirs Anufrijevs <vladimirs.anufrijevs@gmail.com>
2024-12-07 10:06:13 +0200

    add hw8

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
@@ -30,6 +32,41 @@ public class Main {
             System.out.print(arr3[i] + ", ");
         }
         System.out.print(arr3[arr3.length - 1]);
+
+        System.out.println();
+
+        System.out.println("Задание 3");
+
+        for (int i = arr1.length - 1; i > 0; i--) {
+            System.out.print(arr1[i] + ", ");
+        }
+        System.out.print(arr1[0]);
+
+        System.out.println();
+
+        for (int i = arr2.length - 1; i > 0; i--) {
+            System.out.print(arr2[i] + ", ");
+        }
+        System.out.print(arr2[0]);
+
+        System.out.println();
+
+        for (int i = arr3.length - 1; i > 0; i--) {
+            System.out.print(arr3[i] + ", ");
+        }
+        System.out.print(arr3[0]);
+
+        System.out.println();
+
+        System.out.println("Задание 4");
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] % 2 != 0) {
+                arr1[i] += 1;
+            }
+        }
+
+        System.out.print(Arrays.toString(arr1));
 
     }
 }
